fa1dbeaefe7e843adce1d172bb9a24adc9447907
Lidia Garcia Torregrosa <lidiagarciatorregrosa@gmail.com>
2025-09-14 14:17:44 +0200

    auto-cambios

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,11 @@
 // Punto de entrada de Webpack. Aquí importas los estilos y “enciendes” la práctica que estés trabajando
 
-// 📂 src/js/main.js
-
-// ==== Importar JS de las prácticas ====
 import "./practice/practice1.js";
 import "./practice/practice2.js";
 import "./practice/practice3.js";
 
-// ==== Importar utilidades ====
 import "./utils/dom.js";
 
-// ==== Importar estilos (SCSS) ====
 import "../scss/base.scss";
 import "../scss/HeaderFooter.scss";
 import "../scss/movie.scss";
