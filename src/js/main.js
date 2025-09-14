05a37125ab4221403620629995c404eb9794411f
Lidia Garcia Torregrosa <lidiagarciatorregrosa@gmail.com>
2025-09-14 14:10:44 +0200

    auto-cambios

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 // Punto de entrada de Webpack. Aquí importas los estilos y “enciendes” la práctica que estés trabajando
 
 import "../../scss/style.scss";
+
 import "./practice/practice1.js";
 import "./practice/practice2.js";
 import "./practice/practice3.js";
