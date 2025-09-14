9a2eca1bce028369340291f4b2ce3b2f5cf2554b
Lidia Garcia Torregrosa <lidiagarciatorregrosa@gmail.com>
2025-09-14 14:14:15 +0200

    auto-cambios

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,6 @@
 
 import "../../scss/style.scss";
 import "./utils/dom.js";
-const PRACTICE_BY_HASH = {
-  1: () => import("./practice/practice1.js"),
-  2: () => import("./practice/practice2.js"),
-  3: () => import("./practice/practice3.js"),
-};
+import "./practice/practice1.js";
+import "./practice/practice2.js";
+import "./practice/practice3.js";
