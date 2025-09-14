4c31fe3fdb9029dd1b5d97a0fe9c53f4193bcb1f
Lidia Garcia Torregrosa <lidiagarciatorregrosa@gmail.com>
2025-09-14 14:16:08 +0200

    auto-cambios

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
 // Punto de entrada de Webpack. Aquí importas los estilos y “enciendes” la práctica que estés trabajando
 
-import "../../scss/style.scss";
-import "./utils/dom.js";
+// main.js
 import "./practice/practice1.js";
 import "./practice/practice2.js";
 import "./practice/practice3.js";
+import "./utils/dom.js";
+
+// SCSS (Webpack los compila a CSS)
+import "../scss/base.scss";
+import "../scss/HeaderFooter.scss";
+import "../scss/movie.scss";
+import "../scss/style.scss";
