639dbe7e1378a4a155faf236f6ca460d25661849
Lidia Garcia Torregrosa <lidiagarciatorregrosa@gmail.com>
2025-09-14 13:53:52 +0200

    chore: stop tracking build and deps

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-// Punto de entrada de Webpack. Aquí importas los estilos y “enciendes” la práctica que estés trabajando.
+// Punto de entrada de Webpack. Aquí importas los estilos y “enciendes” la práctica que estés trabajando
+
+import "../../scss/style.scss";
+import "./practice/practice1.js";
+import "./practice/practice2.js";
+import "./practice/practice3.js";
