8a915a3639031f78dd6c9d1470aeae38f17578b0
Lidia Garcia Torregrosa <lidiagarciatorregrosa@gmail.com>
2025-09-14 14:16:28 +0200

    auto-cambios

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,5 @@
 // Configura loaders (SCSS), servidor de desarrollo y empaquetado
+import "../scss/base.scss";
+import "../scss/HeaderFooter.scss";
+import "../scss/movie.scss";
+import "../scss/style.scss";
